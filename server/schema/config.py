73eec0a1388477c0cf0b0f650431d69158e52f9b
Armaan Jhanji <armaanj32@gmail.com>
2026-02-05 20:12:18 +0400

    feat: align server config with new vars

--- server/schema/config.py
+++ server/schema/config.py
@@ -5,11 +5,20 @@ class ServerSettings(BaseSettings):
     backend_port: int
     debug: bool
     cors_origins: str
+    client_uri: str
 
+    spotify_auth_url: str
+    spotify_token_url: str
     spotify_client_id: str
     spotify_redirect_uri: str
+    spotify_token_scopes: str
 
-    client_uri: str
+    cookie_secret: str
+    cookie_salt: str
+    pkce_cookie: str
+    session_cookie: str
+    pkce_cookie_max_age: int
+    session_cookie_max_age: int
 
     class Config:
         env_file = ".env"
