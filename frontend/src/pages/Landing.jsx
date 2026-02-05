b36d55ae71d954dbec87b88ec9f4c022f923b442
Armaan Jhanji <armaanj32@gmail.com>
2026-02-06 02:54:24 +0400

    refactor: add new authStatus bool and testing useEffect for /spotify/profile endpoint

--- frontend/src/pages/Landing.jsx
+++ frontend/src/pages/Landing.jsx
@@ -4,8 +4,11 @@ import { api } from '../utils/api';
 
 export const Landing = () => {
     const [serverHello, setServerHello] = useState("");
-    const [authStatus, setAuthStatus] = useState("Loading...");
+    const [authStatus, setAuthStatus] = useState(false);
+    const [authMsg, setAuthMsg] = useState("Loading...");
+    const [welcomeMsg, setWelcomeMsg] = useState("Loading...");
     const [loadingLogin, setLoadingLogin] = useState(false);
+    const [userProfile, setUserProfile] = useState({});
 
     // TESTING: Fetch server hello message
     useEffect(() => {
@@ -27,12 +30,15 @@ export const Landing = () => {
             try {
                 const res = await api.get("/auth/status");
                 if (res.authenticated) {
-                    setAuthStatus("You are logged in");
+                    setAuthStatus(true);
+                    setAuthMsg("You are logged in");
                 } else {
-                    setAuthStatus("You are not logged in");
+                    setAuthStatus(false);
+                    setAuthMsg("You are not logged in");
                 }
             } catch (err) {
-                setAuthStatus("Error getting auth status");
+                setAuthStatus(false);
+                setAuthMsg("Error getting auth status");
             }
         })();
     }, []);
@@ -43,12 +49,43 @@ export const Landing = () => {
         window.location.href = `${import.meta.env.VITE_API_BASE_URL || 'http://127.0.0.1:8000'}/auth/login`;
     };
 
+    // TESTING: Get user profile
+    useEffect(() => {
+        (async () => {
+            if (authStatus) {
+                try {
+                    const res = await api.get("/spotify/profile");
+                    if (res) {
+                        setUserProfile(res);
+                    }
+
+                } catch (err) {
+                    setWelcomeMsg("Error getting user profile from Spotify");
+                }
+            }
+        })();
+    }, [authStatus]);
+    
+    // TESTING: Update welcome message and log response object when userProfile is set
+    useEffect(() => {
+        if (userProfile?.name) {
+            setWelcomeMsg(`Welcome ${userProfile.name}`);
+            console.log(userProfile);
+        }
+    }, [userProfile]);
+
+
     return (
         <>
-            <h2>{serverHello}</h2>
+            <h3>{serverHello}</h3>
+
             <div className="card">
-                <p>{authStatus}</p>
-                {authStatus === "You are not logged in" && (
+                {authStatus && userProfile && (
+                    <h2>{welcomeMsg}</h2>
+                )}
+                <p>{authMsg}</p>
+                
+                {!authStatus && (
                     <button onClick={handleLogin} disabled={loadingLogin}>
                         {loadingLogin ? "Redirecting to Spotify..." : "Login with Spotify"}
                     </button>
