bd4609125b2bb946ceb2f1e27b80054f8d0fe72d
Armaan Jhanji <armaanj32@gmail.com>
2026-02-05 12:48:37 +0400

    refactor: update client vars and uri + porting to align with spotify usage

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,7 +1,18 @@
-import { defineConfig } from 'vite'
+import { defineConfig, loadEnv } from 'vite'
 import react from '@vitejs/plugin-react-swc'
 
 // https://vite.dev/config/
-export default defineConfig({
-  plugins: [react()],
+export default defineConfig (({ mode }) => {
+  const env = loadEnv(mode, process.cwd(), "VITE_");
+
+  const HOST = env.VITE_HOST || "127.0.0.1";
+  const PORT = Number(env.VITE_PORT || 5173);
+
+  return {
+    plugins: [react()],
+    server: {
+      host: HOST,
+      port: PORT,
+    },
+  };
 })
