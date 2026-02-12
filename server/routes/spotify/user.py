83b74be92f8f63e53de34a7ac93f062a3b5db631
ShaikRafaan <149193571+ShaikRafaan@users.noreply.github.com>
2026-02-12 18:29:23 +0400

    feat: Added route to fetch users top tracks and artists

--- server/routes/spotify/user.py
+++ server/routes/spotify/user.py
@@ -1,8 +1,11 @@
 import httpx
 from fastapi import APIRouter, Request, HTTPException
-from schema import ServerSettings, ContentSettings, ProfileImage, UserProfile
+from schema import ServerSettings, ContentSettings, ProfileImage, UserProfile , UserTopItems
 from dotenv import load_dotenv
 from utils import InvalidSessionError, AuthenticationError, get_token
+from typing import List, Optional,Literal
+from pydantic import ValidationError
+
 
 
 load_dotenv()
@@ -61,3 +64,29 @@ async def get_profile(request: Request) -> UserProfile:
         user_uri=response["uri"]
     )
 
+@router.get("/top-items",response_model=UserTopItems)
+async def get_top_items(request: Request,
+                        type: Literal["artists", "tracks"],):
+    """
+    Gets the user's top tracks from Spotify.
+    """
+    session_cookie = request.cookies.get(settings.session_cookie)
+    if not session_cookie:
+        raise HTTPException(status_code=404, detail="No session found")
+    
+    try:
+        access_token = get_token(session_cookie)
+    except (InvalidSessionError, AuthenticationError):
+        raise HTTPException(status_code=401, detail="Invalid or expired session")
+
+    headers = {"Authorization": f"Bearer {access_token}"}
+    async with httpx.AsyncClient(timeout=20) as client:
+        res = await client.get(f"{settings.spotify_base_url}/me/top/{type}", headers=headers)
+        if res.status_code != 200:
+            raise HTTPException(status_code=400, detail=f"Failed to fetch top items from Spotify: {res.text}")
+        
+        response = res.json()
+
+    return {
+        "type": type,
+        **response}
