2ad37a16c37994b627bc4eaddc66b00008ca0675
ShaikRafaan <149193571+ShaikRafaan@users.noreply.github.com>
2026-02-12 22:04:14 +0400

    chore: updated init.py with playback router

--- server/routes/spotify/__init__.py
+++ server/routes/spotify/__init__.py
@@ -1,8 +1,10 @@
 from fastapi import APIRouter
 from .user import router as user_router
 from .top_items import router as top_items_router
+from .playback import router as playback_router
 
 router = APIRouter()
 
 router.include_router(user_router, tags=['User'])
 router.include_router(top_items_router, tags=['Top Items'])
+router.include_router(playback_router, tags=['Playback'])
