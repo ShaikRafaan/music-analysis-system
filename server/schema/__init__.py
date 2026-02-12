7232779069356d285b1d014757d755769f1d3767
ShaikRafaan <149193571+ShaikRafaan@users.noreply.github.com>
2026-02-12 22:04:58 +0400

    chore: updated init.py with RecentlyPlayed schema

--- server/schema/__init__.py
+++ server/schema/__init__.py
@@ -1,4 +1,4 @@
 from .config import ServerSettings
 from .server import RootResponse
 from .auth import CookieConfig, StatusResponse, PKCECookie, SpotifyTokenResponse
-from .spotify import ContentSettings, ProfileImage, UserProfile ,UserTopItems
+from .spotify import ContentSettings, ProfileImage, UserProfile ,UserTopItems,RecentlyPlayed
