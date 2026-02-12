9029f46c3c9127543c10ce39610fc2fe0b43a68d
ShaikRafaan <149193571+ShaikRafaan@users.noreply.github.com>
2026-02-12 20:12:29 +0400

    chore: updated init.py file

--- server/schema/spotify/__init__.py
+++ server/schema/spotify/__init__.py
@@ -1,1 +1,3 @@
-from .user import ContentSettings, ProfileImage, UserProfile ,UserTopItems
+from .user import ContentSettings, ProfileImage, UserProfile
+from .top_items import ProfileImage, ExternalUrls, Restrictions, Followers, Album, Artist, ArtistObjects,TrackObjects,UserTopItems
+
