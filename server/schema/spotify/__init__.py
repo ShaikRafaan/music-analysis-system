97d5f97ab9d49ae3b5786f945901b7fd5fde2cd3
ShaikRafaan <149193571+ShaikRafaan@users.noreply.github.com>
2026-02-12 22:05:47 +0400

    chore: updated spotify init.py with RecentlyPlayed schema

--- server/schema/spotify/__init__.py
+++ server/schema/spotify/__init__.py
@@ -1,3 +1,4 @@
 from .user import ContentSettings, ProfileImage, UserProfile
 from .top_items import ProfileImage, ExternalUrls, Restrictions, Followers, Album, Artist, ArtistObjects,TrackObjects,UserTopItems
+from .playback import RecentlyPlayed
 
