03bf1980945f1ceffa15405484ef9ab977170efd
ShaikRafaan <149193571+ShaikRafaan@users.noreply.github.com>
2026-02-12 20:12:14 +0400

    chore: updated init.py file

--- server/routes/spotify/__init__.py
+++ server/routes/spotify/__init__.py
@@ -1,6 +1,8 @@
 from fastapi import APIRouter
 from .user import router as user_router
+from .top_items import router as top_items_router
 
 router = APIRouter()
 
 router.include_router(user_router, tags=['User'])
+router.include_router(top_items_router, tags=['Top Items'])
